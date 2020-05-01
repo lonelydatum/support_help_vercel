dae85477f3859c791165e84cf38da4e4793d27c6
Gar <gar@lonelydatu.com>
2020-05-01 11:34:22 -0400

    on

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+
+class index extends Component {
+    render () {
+        return (
+            <MainCSS>
+            asdfasdfsdf
+            </MainCSS>
+        )
+    }
+}
+
+
+const MainCSS = styled.div`
+    display: flex;
+    
+`
+export default index
